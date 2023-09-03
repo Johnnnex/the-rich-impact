55ffcdc3e9d5fe6460b3ee0bf0e9032c4c1c5714
Johnnnex <onyeiborjohn05@gmail.com>
2023-09-03 23:24:27 +0100

    header

--- therichimpact/layout/Header.jsx
+++ therichimpact/layout/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
                 <Image alt="logo-img" height={1} width={100} className="" src="/images/site-logo.svg" />
             </div>
             <nav className="flex items-center text-white gap-[74px]">
-                <ul className="text-[.8rem] font-[500] font-clashGrotesk gap-[32px] flex">
+                <ul className="text-[15px] font-[500] font-clashGrotesk gap-[32px] flex">
                     <li className="">OUR EXPERTISE</li>
                     <li className="">PORTFOLIO</li>
                     <li className="">OUR TEAM</li>
                     <li className="">FAQS</li>
                 </ul>
-                <button className="text-[.8rem] font-[700] font-clashGrotesk w-[150px] rounded-[8px] py-[10px] bg-[#FCA311]">
+                <button className="text-[15px] font-[700] font-clashGrotesk w-[200px] rounded-[8px] py-[10px] bg-[#FCA311]">
                     WORK WITH US
                 </button>
             </nav>
