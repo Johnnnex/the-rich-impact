f02f02b159569fda090fb49358b4833083951954
Johnnnex <onyeiborjohn05@gmail.com>
2023-09-03 22:55:19 +0100

    nav

--- therichimpact/layout/Header.jsx
+++ therichimpact/layout/Header.jsx
@@ -3,21 +3,23 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header className="mx-[124px] flex justify-between">
-        <div className="">
-            <Image alt="logo-img" height={1} width={100} className="fixed" src="/images/site-logo.svg" />
+    <header className="py-[13px] bg-[#01020F] ">
+        <div className="w-[90%] mx-auto flex justify-between">
+            <div className="">
+                <Image alt="logo-img" height={1} width={100} className="" src="/images/site-logo.svg" />
+            </div>
+            <nav className="flex items-center text-white gap-[74px]">
+                <ul className="text-[.8rem] font-[500] font-clashGrotesk gap-[32px] flex">
+                    <li className="">OUR EXPERTISE</li>
+                    <li className="">PORTFOLIO</li>
+                    <li className="">OUR TEAM</li>
+                    <li className="">FAQS</li>
+                </ul>
+                <button className="text-[.8rem] font-[700] font-clashGrotesk w-[150px] rounded-[8px] py-[10px] bg-[#FCA311]">
+                    WORK WITH US
+                </button>
+            </nav>
         </div>
-        <nav className="flex items-center">
-            <ul className="text-[14px] font-[500] font-clashGrotesk gap-[32px] flex">
-                <li className="">OUR EXPERTISE</li>
-                <li className="">PORTFOLIO</li>
-                <li className="">OUR TEAM</li>
-                <li className="">FAQS</li>
-            </ul>
-            <button className="text-[14px] font-[700] font-clashGrotesk w-[200px] rounded-[16px] py-[20px] bg-[#FCA311]">
-                WORK WITH US
-            </button>
-        </nav>
     </header>
   )
 }
