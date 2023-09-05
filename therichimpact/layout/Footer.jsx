4f3ebafbc793661277183d2df0334368510b3f83
Johnnnex <onyeiborjohn05@gmail.com>
2023-09-05 02:35:09 +0100

    swiper

--- therichimpact/layout/Footer.jsx
+++ therichimpact/layout/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className="py-[70px] bg-[#01020F]">
+      <section className="w-[85%] flex justify-between items-center mx-auto">
+        <div className="">
+          <Image alt="logo-img" height={1} width={100} className="" src="/images/site-logo.svg" />
+        </div>
+        <p className="text-white font-clashGrotesk text-[18px] font-[500]">
+          2023 The Rich Impact. All rights reserved
+        </p>
+        <div className="flex gap-[12px]">
+          <Image alt="social-img" height={1} width={30} className="" src="/images/Twitter.svg" />
+          <Image alt="social-img" height={1} width={30} src="/images/lnkdn.svg" />
+        </div>
+      </section>
+    </footer>
   )
 }
 
--- therichimpact/pages/component/index.jsx
+++ therichimpact/pages/component/index.jsx
@@ -1,4 +1,11 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { A11y, Autoplay, Pagination } from "swiper/modules"
+import { Rating } from 'react-simple-star-rating'
+import "swiper/css/autoplay"
+import 'swiper/css/pagination'
+import "swiper/css/bundle"
+import 'swiper/css'
 import Image from 'next/image'
 import HeadComp from '@/layout/HeadComp'
 
@@ -230,6 +237,123 @@ const Index = () => {
                 <Image src="/images/circle.svg" className="w-[90px] mx-auto h-[100px]" alt="partners" height={1} width={200} />
             </div>
             <h3 className="text-[#01020F] font-clashGrotesk mt-[91px] text-[48px] font-[500] text-center mb-[42px]">TESTIMONIALS</h3>
+            <section className="bg-[#110c32a7] mt-[7rem] py-[5rem]">
+                <div className="w-[90%] mx-auto">
+                    <h2 className="font-poppins font-[700] text-[3rem]">What our clients say</h2>
+                    <p className=" font-robotoMono font-thin w-[50%] mt-[1rem] mb-[3rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea error excepturi nostrum? Odio suscipit tempore, et quaerat earum architecto </p>
+                    <Swiper
+                        className="h-fit"
+                        grabCursor={true}
+                        pagination={{
+                            dynamicBullets: true,
+                            clickable: true,
+                        }}
+                        modules={[A11y, Autoplay, Pagination]}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        onAutoplayTimeLeft={onAutoplayTimeLeft}
+                        spaceBetween={50}
+                        slidesPerView={3}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        >
+                            <SwiperSlide className='bg-[#808080] px-5 py-7 rounded-[8px]'>
+                                <div className="">
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                </div>
+                                <p className="mb-[1rem] font-robotoMono">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo excepturi accusamus, molestiae iure voluptate officiis harum consequatur ratione magni ullam ex quaerat, voluptatem, quos facilis corporis consectetur deleniti.</p>
+                                <div className="flex justify-between">
+                                    <Image src="/Images/pfp-imgg.jpg" className='rounded-[50%]' alt="pfp-img" height={1} width={50} />
+                                    <p className="w-[80%] font-robotoMono">
+                                        <b className="font-poppins">John Doe</b> <br />
+                                        M.D, Johnex corp.
+                                    </p>
+                                </div>
+
+                            </SwiperSlide>
+                            <SwiperSlide className='bg-[#808080] px-5 py-7 rounded-[8px]'>
+                                <div className="">
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                </div>
+                                <p className="mb-[1rem] font-robotoMono">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo excepturi accusamus, molestiae iure voluptate officiis harum consequatur ratione magni ullam ex quaerat, voluptatem, quos facilis corporis consectetur deleniti.</p>
+                                <div className="flex justify-between">
+                                    <Image src="/Images/pfp-imgg.jpg" className='rounded-[50%]' alt="pfp-img" height={1} width={50} />
+                                    <p className="w-[80%] font-robotoMono">
+                                        <b className="font-poppins">John Doe</b> <br />
+                                        M.D, Johnex corp.
+                                    </p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className='bg-[#808080] px-5 py-7 rounded-[8px]'>
+                                <div className="">
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                </div>
+                                <p className="mb-[1rem] font-robotoMono">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo excepturi accusamus, molestiae iure voluptate officiis harum consequatur ratione magni ullam ex quaerat, voluptatem, quos facilis corporis consectetur deleniti.</p>
+                                <div className="flex justify-between">
+                                    <Image src="/Images/pfp-imgg.jpg" className='rounded-[50%]' alt="pfp-img" height={1} width={50} />
+                                    <p className="w-[80%] font-robotoMono">
+                                        <b className="font-poppins">John Doe</b> <br />
+                                        M.D, Johnex corp.
+                                    </p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className='bg-[#808080] px-5 py-7 rounded-[8px]'>
+                                <div className="">
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={0} readonly={true} iconsCount={1}/>
+                                </div>
+                                <p className="mb-[1rem] font-robotoMono">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo excepturi accusamus, molestiae iure voluptate officiis harum consequatur ratione magni ullam ex quaerat, voluptatem, quos facilis corporis consectetur deleniti.</p>
+                                <div className="flex justify-between">
+                                    <Image src="/Images/pfp-imgg.jpg" className='rounded-[50%]' alt="pfp-img" height={1} width={50} />
+                                    <p className="w-[80%] font-robotoMono">
+                                        <b className="font-poppins">John Doe</b> <br />
+                                        M.D, Johnex corp.
+                                    </p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className='bg-[#808080] px-5 py-7 rounded-[8px]'>
+                                <div className="">
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                    <Rating initialValue={1} readonly={true} iconsCount={1}/>
+                                </div>
+                                <p className="mb-[1rem] font-robotoMono">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias obcaecati illo excepturi accusamus, molestiae iure voluptate officiis harum consequatur ratione magni ullam ex quaerat, voluptatem, quos facilis corporis consectetur deleniti.</p>
+                                <div className="flex justify-between">
+                                    <Image src="/Images/pfp-imgg.jpg" className='rounded-[50%]' alt="pfp-img" height={1} width={50} />
+                                    <p className="w-[80%] font-robotoMono">
+                                        <b className="font-poppins">John Doe</b> <br />
+                                        M.D, Johnex corp.
+                                    </p>
+                                </div>
+                            </SwiperSlide>
+                            <div className="autoplay-progress" slot="container-end">
+                                <svg viewBox="0 0 48 48" ref={progressCircle}>
+                                    <circle cx="24" cy="24" r="20"></circle>
+                                </svg>
+                                <span ref={progressContent}></span>
+                            </div>
+                    </Swiper>
+                </div>
+            </section>
         </section>
     </>
   )
