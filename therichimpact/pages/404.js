37f6195e4f04e00e429d5afb391b643b05ab7247
Johnnnex <onyeiborjohn05@gmail.com>
2023-09-18 16:51:53 +0100

    404

--- therichimpact/pages/404.js
+++ therichimpact/pages/404.js
@@ -7,7 +7,7 @@ const Error = () => {
   return (
     <>
         <HeadComp title="The Rich Impact - 404" />
-        <section className="bg-[#01020F] pt-[50px] pb-[20px]">
+        <section className="bg-[#01020F] pt-[50px] pb-[20px] md:w-[90%] mx-auto">
             <Image src="/images/err.svg" alt="404" className="w-fit mx-auto wow fadeInDown mb-[32px]" height={1} width={450} />
             <h3 className="text-white text-center text-[32px] wow fadeIn font-[400] font-satoshi">Oops!  Page Not Found!</h3>
             <p className="text-white font-satoshi mt-[16px] min-w-[20rem] text-[18px] wow fadeIn w-[50%] mx-auto text-center">
